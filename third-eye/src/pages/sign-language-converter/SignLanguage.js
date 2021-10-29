a0e22ce6a9d411084cd29f4d26a3f6b06a61fa63
SAHARIPRASAD-2907 <67941813+SAHARIPRASAD-2907@users.noreply.github.com>
2021-10-29 08:49:30 +0530

    adding CSS to all pages

--- third-eye/src/pages/sign-language-converter/SignLanguage.js
+++ third-eye/src/pages/sign-language-converter/SignLanguage.js
@@ -1,75 +1,77 @@
-import React,{useState} from 'react'
-import ReactPlayer from "react-player"
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import axios from "axios";
-const FileDownload = require('js-file-download');
-
-
+const FileDownload = require("js-file-download");
+import "sign-language-converter.scss";
 
 const SignLanguage = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition()
-  const [state,setState] = useState("");
-  const [status,setStatus] = useState("")
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [state, setState] = useState("");
+  const [status, setStatus] = useState("");
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return null
+    return null;
   }
 
-  const generateVideo =()=>{
-    console.log("string:"+state);
-    const fd = new FormData()
-    fd.append("speech",transcript)
-    if(state){
-        axios.post("http://127.0.0.1:5001/upload2",fd).then(
-        (res)=>{
-            if(res.data){
-                setStatus(res.data);
-                console.log("Received Data-"+status);
-            }  
+  const generateVideo = () => {
+    console.log("string:" + state);
+    const fd = new FormData();
+    fd.append("speech", transcript);
+    if (state) {
+      axios.post("http://127.0.0.1:5001/upload2", fd).then(
+        (res) => {
+          if (res.data) {
+            setStatus(res.data);
+            console.log("Received Data-" + status);
+          }
         },
-        (err)=>{
-            console.log(err);
+        (err) => {
+          console.log(err);
         }
-    )
+      );
     }
-    if(status){
-        axios.get("http://127.0.0.1:3002/receive").then(
-            (res)=>{
-                console.log("data received sucessfully");
-                FileDownload(res.data, 'test.mp4');
-            }
-        )
+    if (status) {
+      axios.get("http://127.0.0.1:3002/receive").then((res) => {
+        console.log("data received sucessfully");
+        FileDownload(res.data, "test.mp4");
+      });
     }
-  }
-  
+  };
 
   return (
     <div>
-        <h1>Text to sign Language</h1>
-      <button onClick={()=>{
+      <h1>Text to sign Language</h1>
+      <button
+        onClick={() => {
           SpeechRecognition.startListening();
-      }}>Start</button>
-      <button onClick={()=>{
-          SpeechRecognition.stopListening();
-          setState(transcript)
-      }}>
-      Stop
+        }}
+      >
+        Start
       </button>
-      <button onClick={()=>{
+      <button
+        onClick={() => {
+          SpeechRecognition.stopListening();
+          setState(transcript);
+        }}
+      >
+        Stop
+      </button>
+      <button
+        onClick={() => {
           resetTranscript();
-          setState("")
-      }}>Reset</button>
+          setState("");
+        }}
+      >
+        Reset
+      </button>
       <p>{transcript}</p>
       <button onClick={generateVideo}>genrate</button>
-      <br/>
-      <ReactPlayer 
-        url = "https://youtu.be/xHs4qyyDSqs"
-        controls = {true}
-      />
-      
-
-      
+      <br />
+      <ReactPlayer url="https://youtu.be/xHs4qyyDSqs" controls={true} />
     </div>
-  )
-}
+  );
+};
 export default SignLanguage;
